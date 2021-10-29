bbb8324318908e1361c1e69d9e450106fb865656
Arrowana <8245419+Arrowana@users.noreply.github.com>
2021-10-29 16:02:20 +1100

    Updates

---
+++ _next/static/jKUtA3EtOrHK_DasfkdwH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-84c28100cefa02f6d333.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
